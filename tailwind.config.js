6fa85d588eb35e82eb6fb47c9f05c99a859290f0
Abdur Rehman Majeed <abdurrehman712@hotmail.com>
2023-07-09 19:28:19 +0500

    Layout Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./public/**/*.{html,js}"],
   theme: {
     container: {
@@ -14,10 +15,13 @@ module.exports = {
     },
     fontFamily: {
       'display': ['Poppins', 'sans-serif'],
-      'body': ['Roboto', 'sans-serif'],
-      // 'jameel': ['jameel-noori-nastaleeq'],
+      'body': ['Poppins', 'sans-serif'],
+      'urdu': ['Noto Nastaliq Urdu', 'serif'],
     },
     extend: {
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+      },
       fontSize: {
         '2xs': ['0.625rem', '0.75rem'], // 10px
         '3xs': ['0.5rem', '0.625rem'], // 8px
